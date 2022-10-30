aa7d58483492356069789aa166bfbc012d058f6b
bloome-alex <alex99valdiviezo@gmail.com>
2022-10-30 13:39:18 -0300

    initializing fixed

---
+++ src/modules/models/Product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Product = mongoose.model('product', { 
+    image: {type: String, unique: false, required: true},
+    title: {type: String, unique: true, required: true},
+    description: {type: String, unique: false, required: false},
+    price: {type: String, unique: false, required: true},
+    // stock: {type: Number, unique: false, required: false, default: ''} //TODO: ¿Es necesario?
+    category: {type: String, unique: false, enum:['ALIMENTO'], required: true}
+})
+
+module.exports = Product
---
+++ src/modules/services/index.js
@@ -0,0 +1,37 @@
+//UserServices
+
+const {
+fetchUsers,
+paginateUsers,
+createUser,
+updateUser,
+authUser,
+authUserByToken,
+existUser } = require('./userServices')
+
+//ProductServices
+
+const {
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    getProducts,
+    getFilterProducts,
+    uploadImage
+} = require('./productService')
+
+module.exports = {
+    fetchUsers,
+    paginateUsers,
+    createUser,
+    updateUser,
+    authUser,
+    authUserByToken,
+    existUser,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    getProducts,
+    getFilterProducts,
+    uploadImage
+}
