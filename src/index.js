b547d9e7912f517256c6b40dc59447da24b2d49b
bloome-alex <alex99valdiviezo@gmail.com>
2022-10-30 12:52:34 -0300

    version 1.0.0 fixed

---
+++ src/index.js
@@ -0,0 +1,58 @@
+require('dotenv').config()
+const express = require('express')
+const {ApolloServer} = require('apollo-server-express')
+const typeDefs = require('./modules/graphql/types')
+const resolvers = require('./modules/graphql/resolvers')
+const cloudinary = require('./utils/cloudinary')
+const upload = require('./utils/multer')
+const { response } = require('express')
+
+class Server{
+    constructor(){
+        this.app = express()
+        this.app.use(express.static('public'))
+        this.apolloServer = new ApolloServer({typeDefs, resolvers})
+        this.init()
+        this.apolloServerStart()
+        this.apiroutes()
+    }
+
+    init(){
+        require('./init')
+    }
+
+    apolloServerStart = async() => {
+        await this.apolloServer.start()
+        this.apolloServer.applyMiddleware({app: this.app})
+    }
+
+    apiroutes(){
+        this.app.post('/api/upload', upload.single('file'), async(req, res) => {
+            //return res.send('hola')
+            try {
+                if(!req.file) throw new Error('undefined file')
+                const {url = ''} = await cloudinary.uploader.upload(req.file.path, {
+                    folder: 'Images'
+                });
+                res.status(202).json({
+                    url
+                })    
+            } catch (error) {
+                response.status(402).json({
+                    msg: 'bad gateway',
+                    error
+                })
+            }
+        })
+    }
+
+    listen(port){
+        this.app.listen(port, () => {
+            console.log(`listen at port ${port}`)
+        })
+    }
+}
+
+const server = new Server()
+
+server.listen(process.env.PORT)
