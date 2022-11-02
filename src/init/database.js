d722ebf8a0e7db792fd6f3175befc964b34b7968
bloome-alex <alex99valdiviezo@gmail.com>
2022-11-02 19:20:28 -0300

    delete all

--- src/init/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DB_MONGO, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: true
-})
-
-console.log('database connected')
