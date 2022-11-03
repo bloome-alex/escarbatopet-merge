940f21c1e318d56ca03879da067c40c5bcebfbff
bloome-alex <alex99valdiviezo@gmail.com>
2022-11-03 18:08:06 -0300

    version 1.6.14

---
+++ src/init/categoriesInit.js
@@ -0,0 +1,17 @@
+const Category = require('../modules/models/Category')
+
+const categories = ['TODOS', 'ALIMENTO', 'INDUMENTARIA', 'ACCESORIOS', 'JUGUETES', 'PRODUCTOS VETERINARIOS', 'PLANTAS', 'MACETAS']
+
+const initCategories = async() => {
+    const isCategory = await Category.find({})
+    if(isCategory.length > 0){
+        await Category.updateOne({id:isCategory._id}, {categories})
+    }else{
+        await Category.create({
+            categories 
+        })
+    }
+    console.log('Categories are created')
+}
+
+initCategories()
